60625f20e030573f1371e6489fb85cdbb08a4127
kathrynsayer <kssayer@gmail.com>
2022-08-15 13:50:10 -0700

    partial project

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+
+for (let num = 1; num <= 100; num++) {
+    if (num % 2 == 1) {
+        console.log(num)
+    } else if (num % 2 == 0) {
+        continue
+    }}
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+
+let x = 1
+for (x = 1; x <= 100; x++) {
+    if (x % 3 == 0 && x % 5 == 0) {
+        console.log("FIZZBUZZ", x);
+    } else if (x % 3 == 0) {
+        console.log("FIZZ", x);
+    } else if (x % 5 == 0) {
+        console.log("BUZZ", x);
+    }}
+
+// Exercise 3 Section
+
+let k = 1
+while (k <=100){
+    if (k % 2 == 1) {
+    console.log(k);}
+    k++;
+
+    }
+
